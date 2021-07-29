251ee25d25b855bf7562e4f47550ad6db82d497d
Matec12 <dejalayo@gmail.com>
2021-07-29 14:01:21 +0100

    commit

---
+++ Ts basics and configuration/dist/unknown-never.js
@@ -0,0 +1,15 @@
+"use strict";
+// let userInput: unknown;
+// let userName: string;
+// userInput = 5;
+// userInput = 'Max';
+// if (typeof userInput === 'string') {
+//   userName = userInput;
+// }
+// //never 
+// function generateError(message: string, code: number): never {
+//   throw { message: message, errorCode: code };
+//   // while (true) {}
+// }
+// generateError('An error occurred!', 500);
+//# sourceMappingURL=unknown-never.js.map
--- Ts basics and configuration/src/functions.ts
+++ Ts basics and configuration/src/functions.ts
@@ -35,33 +35,35 @@
 // let someValue: undefined;
 
 
-//
+// Callbacks
 
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
 
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
+// function printResult(num: number): void {
+//   console.log('Result: ' + num);
+// }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
+// function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+//   const result = n1 + n2;
+//   cb(result);
+// }
 
-printResult(add(5, 12));
+// printResult(add(5, 12));
 
-let combineValues: (a: number, b: number) => number;
+// let combineValues: (a: number, b: number) => number;
 
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
+// combineValues = add;
+// // combineValues = printResult;
+// // combineValues = 5;
 
-console.log(combineValues(8, 8));
+// console.log(combineValues(8, 8));
 
-// let someValue: undefined;
+// // let someValue: undefined;
 
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+// addAndHandle(10, 40, (result) => {
+//   console.log(result);
+//   // return result;
+
+// }); 
